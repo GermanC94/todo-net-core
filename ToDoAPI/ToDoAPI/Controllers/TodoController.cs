0f758adfa3e078f51c911078e05057a2973193d3
GermanC94 <122814315+GermanC94@users.noreply.github.com>
2023-01-16 10:46:55 -0600

    Project created and Swagger added
    This is the first Pull Request where first code is added for API and also Swagger configuration for listing the end-points is configured.

---
+++ ToDoAPI/ToDoAPI/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTodoList()
+        {
+            return Ok();
+        }
+    }
+}
